0300eb68b909e53b7063e094e06b545c4f2c45ed
rootz491 <experimentyt1@gmail.com>
2021-04-30 22:38:10 +0530

    correction on fs.fileWrite()

--- first/app5.js
+++ first/app5.js
@@ -7,7 +7,8 @@ fs.writeFileSync("newFile.txt", readme);
 
 //  async way to read/write files
 fs.readFile("readme.md", "utf8", function(err, data) {
-    fs.writeFile("newFile1.txt", data);
-    console.log("\nData written successfully!");
+    fs.writeFile("newFile.txt", data, (err)=>{ });
+    console.log("\nData written successfully");
+    if(err)    console.error(err);
 });
 
