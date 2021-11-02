8d85b92c4aba54873da9bb947d9e37c2fe56af13
rootz491 <experimentyt1@gmail.com>
2021-11-03 00:17:45 +0530

    :bug::hammer: yey

--- mysql/app.js
+++ mysql/app.js
@@ -1,28 +1,33 @@
 const express = require('express');
-const sql = require('mssql');
+const sql = require('mysql2');
 require('dotenv').config();
 
 const config = {
     user: process.env.username,
     password: process.env.password,
-    server: process.env.server,
+    host: process.env.server,
     database: process.env.database,
     port: 3306    
 }
 
+const connection = sql.createConnection(config);
 
-const pool1 = new sql.ConnectionPool(config);
-pool1.connect()
-    .then(_ => console.log('connected to sql'))
-    .catch(err => console.log('error while connecting: ', err));
+connection.connect();
 
 
 const app = express();
 
-// app.get('/', (req, res) => {
 
-//     res.end();
-// })
+app.get('/', (_, res) => {
+    try {
+        connection.query('SELECT * FROM pet', function (error, results, fields) {
+            if (error) throw error;
+            res.send(results);
+        });
+    } catch (error) {
+        res.status(403).send(error);
+    }
+})
 
 
 app.listen(3000);
--- mysql/readme.md
+++ mysql/readme.md
@@ -4,3 +4,24 @@ trying to understand SQL database with code!
 
 *   tried using it thru commandline and leart quiet a bit, now thinking about using it in a simple webapp as database.
 *   Will aim to learn some commands of mysql, maybe will get handy in interviews someday 😉
+
+
+
+##  some queries
+
+*   create new database
+    ```sql
+    CREATE DATABASE world;
+    ```
+
+*   create new table
+    ```sql
+    CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20),
+        species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+    ```
+
+*   check table metadata/schema
+    ```sql
+    DESCRIBE pet;
+    ```
+
